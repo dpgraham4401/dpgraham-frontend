3f863694c69be8f620a04f3c5cbdb42d54db3c55
David Graham <dpgraham4401@gmail.com>
2023-07-05 16:32:32 -0400

    refactor Card into ArticleCard component

--- src/components/ArticlesOverview/ArticlesGrid.tsx
+++ src/components/ArticlesOverview/ArticlesGrid.tsx
@@ -1,49 +1,25 @@
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Grid,
-  Link,
-  Typography,
-} from "@mui/material";
+import { Grid } from "@mui/material";
+import { ArticleCard } from "components/ArticlesOverview/ArticleCard";
 import { Article } from "features/Articles";
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
 
 interface ArticlesGridProps {
   articles: Article[];
 }
 
+/**
+ * Renders a grid of articles
+ * @param articles
+ * @constructor
+ */
 export function ArticlesGrid({ articles }: ArticlesGridProps) {
+  // For future reference, we could do things like client side filtering here
   return (
     <>
       {articles.map((article) => {
         return (
           <Grid item xs={12} sm={6} md={4} key={article.id}>
-            <Card sx={{ height: 400 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                height="140"
-                image="/static/cool-background.png"
-              />
-              <CardContent>
-                {/* ToDo: replace with an ArticleCard component*/}
-                <Link
-                  to={`${article.id}`}
-                  underline="none"
-                  component={RouterLink}
-                >
-                  <Typography gutterBottom variant="h5" component="div">
-                    {article.title}
-                  </Typography>
-                </Link>
-                <Typography variant="subtitle1" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </Card>
+            <ArticleCard article={article} />
           </Grid>
         );
       })}
