00d46d03b3be0132ddf2730244d98d6a11a30135
David Graham <dpgraham4401@gmail.com>
2023-07-05 16:47:42 -0400

    ArticleGrid test suite

--- src/components/ArticlesOverview/ArticleGrid.spec.tsx
+++ src/components/ArticlesOverview/ArticleGrid.spec.tsx
@@ -1,11 +1,36 @@
 import { render, screen } from "@testing-library/react";
-import { ArticlesOverview } from "components/ArticlesOverview/ArticlesOverview";
+import { ArticlesGrid } from "components/ArticlesOverview/ArticlesGrid";
+import { Article } from "features/Articles";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-describe("Article List", () => {
-  it("renders all the articles", () => {
-    render(<ArticlesOverview />);
-    const titleElement = screen.getByText(/coming soon/i);
-    expect(titleElement).toBeInTheDocument();
+const mockArticles: Article[] = [
+  {
+    id: 1,
+    content: "# Hello World",
+    title: "Hello World",
+    updateDate: new Date(),
+    createDate: new Date().toDateString(),
+  },
+  {
+    id: 2,
+    content: "# foo bar",
+    title: "foo bar",
+    updateDate: new Date(),
+    createDate: new Date().toDateString(),
+  },
+];
+
+describe("Articles Grid", () => {
+  it("renders a number of cards", () => {
+    render(
+      <BrowserRouter>
+        <ArticlesGrid articles={mockArticles} />
+      </BrowserRouter>
+    );
+    mockArticles.forEach((article) => {
+      const titleElement = screen.getByText(article.title);
+      expect(titleElement).toBeInTheDocument();
+    });
   });
 });
