0fbc5b811e959b074b13640e40fe0f94cad97623
David Graham <dpgraham4401@gmail.com>
2023-07-05 18:26:48 -0400

    scaffold resume routing and component

---
+++ src/components/CloudChallengeResume/CloudChallengeResume.tsx
@@ -0,0 +1,16 @@
+import { Card } from "@mui/material";
+import React from "react";
+
+/**
+ * This Resume is, primarily in HTML and css as part of the cloud resume challenge.
+ * @constructor
+ */
+export function CloudChallengeResume() {
+  return (
+    <Card sx={{ backgroundColor: "white" }}>
+      <div>
+        <h1>David Graham</h1>
+      </div>
+    </Card>
+  );
+}
